b713d7804378ba2d80da79b7d8f7ea64f28df74a
Ángel Raúl Recio Guerrero <redzero2930@gmail.com>
2023-03-01 16:36:29 -0600

    Hook Routing

--- src/App.js
+++ src/App.js
@@ -3,27 +3,18 @@ import Navbar from './components/navbar';
 import Pricing from './pages/Pricing';
 import About from './pages/About';
 import Home from './pages/Home';
+import {Route, Routes} from "react-router-dom"
 
 function App() {
-  
-  let component
-  switch(window.location.pathname){
-    case "/":
-      component = <Home />
-    break
-    case "/pricing":
-      component = <Pricing />
-    break
-    case "/about":
-      component = <About />
-    break
-  }
-
   return (
     <div className="App">
         <Navbar/>
         <div className="container">
-            {component} 
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/pricing' element={<Pricing />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
         </div>
     </div>
   );
